85837ad6f6e980f90d4284108ae8f2db4241dcd5
Nestor Taborda <nestor195.nt@gmail.com>
2026-03-19 10:50:22 -0300

    compatibilidad con githubpages

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ initUI();
 // Register Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(registration => {
+    navigator.serviceWorker.register('./sw.js').then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch(err => {
       console.error('Service Worker registration failed:', err);
