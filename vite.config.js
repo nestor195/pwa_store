440f07055fbae2e244998ff048b8b9f90372db26
Nestor Taborda <nestor195.nt@gmail.com>
2026-03-19 10:16:35 -0300

    eliminacion de tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [tailwindcss()],
+    plugins: [],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
